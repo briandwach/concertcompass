e18ee8575a04be626b8d543f0e157d262bfc90b4
livefromthelevel <brian.d.wach@gmail.com>
2024-09-29 16:01:53 -0500

    Cleaned up server project files, cleaned up, client project files, and create favicon

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,15 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3006,
     open: true,
-   // We have to set up our Vite client-side development server to proxy API requests to our server-side Node server at port 3001 in order to hit our GraphQL application.
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },     
-    }
   }
 })
