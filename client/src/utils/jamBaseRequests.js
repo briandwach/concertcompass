4a83e6f21517202ffe17b42a28ba3b7f92856e03
livefromthelevel <brian.d.wach@gmail.com>
2024-11-27 18:25:56 -0600

    Homepage pull metro data from db and renders metro areas as list elements.

--- client/src/utils/jamBaseRequests.js
+++ client/src/utils/jamBaseRequests.js
@@ -1,7 +1,31 @@
+export const getMetros = async () => {
+    try {
+        const res = await fetch('/api/jamBase/metros', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!res.ok) {
+            const errorData = await res.json();
+            throw new Error(`HTTP error! Status: ${res.status} - ${errorData.message || errorData}`);
+        }
+        
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error('Error retrieving metro areas from server', error);
+    }
+}
+
+
+// ADMINISTRATION
+// Updates metro areas in database to match that of JamBases
 export const updateMetros = async () => {
     try {
         const res = await fetch('/api/jamBase/metros', {
-            method: 'POST',
+            method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
             }
--- server/controllers/api/jamBaseRoutes.js
+++ server/controllers/api/jamBaseRoutes.js
@@ -1,8 +1,33 @@
 const router = require('express').Router();
 const { concatEp, createOptions, updateMetroAreas } = require('../../utils/jamBaseUtils');
+const { Metros } = require('../../models');
 
-router.post('/metros', async (req, res) => {
+// Gets array of JamBase metro data and delivers to client
+router.get('/metros', async (req, res) => {
+    try {
+        // Await the database query
+        const metros = await Metros.find({});
 
+        if (metros.length === 0) {
+            console.error('Requested metros not found in the database.');
+            return res.status(404).json({ error: 'Metro data not found' });
+        }
+        
+        // Return the list of metros with 200 OK status
+        return res.status(200).json(metros);
+    } catch (err) {
+        // Catch any error during the query
+        console.error('Error retrieving metros data from db', err);
+        
+        // Return a 500 internal server error response
+        res.status(500).json({ error: 'Something went wrong, please refresh and try again.' });
+    }
+});
+
+
+// ADMINISTRATION
+// Updates metro areas in database to match that of JamBases
+router.put('/metros', async (req, res) => {
     try {
         const updateMetros = await updateMetroAreas();
 
@@ -14,8 +39,7 @@ router.post('/metros', async (req, res) => {
     } catch (err) {
         console.error('Error updating metros in database:', err);
         res.status(500).json({ error: 'Something went wrong, please try again.' });
-    }
-    
+    }  
 });
 
 module.exports = router;
